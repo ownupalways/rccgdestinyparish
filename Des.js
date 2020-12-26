9684c28b22430a1a5b617876a5d841e6c7b9f51c
Oluwadipe Godwin Jesuropo <65556599+ownupalways@users.noreply.github.com>
2020-12-26 07:31:02 -0800

    New files add-ups

---
+++ Des.js
@@ -0,0 +1,83 @@
+        //fetching Modal Box
+        var modal = document.getElementById('ModalForm');
+
+        //when user click anywhere utside the modal box, it will close
+
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display= "none";
+            }
+        }
+
+        
+        function viewLogin() {
+            let x = document.getElementById("Pword");
+            if (x.type === "Password") {
+                x.type = "text";
+            }else{
+                x.type = "Password";
+            }
+        }
+        
+        $(function() {
+            $(".tog1").click(function(){
+                $(".batch").slideToggle(500);
+            });
+        });
+        $(function() {
+            $(".close-batch").click(function(){
+                $(".batch").hide(500);
+            });
+        });     
+        $(function() {
+            $(".tog2").click(function(){
+                $(".payment").slideToggle(500);
+            });
+        });
+        $(function() {
+            $(".close-payment").click(function(){
+                $(".payment").hide(500);
+            });
+        });
+        
+         $(function() {
+            $(".tog3").click(function(){
+                $(".region").slideToggle(500);
+            });
+        });
+        $(function() {
+            $(".close-region").click(function(){
+                $(".region").hide(500);
+            });
+        });  
+        $(function() {
+            $(".tog4").click(function(){
+                $(".zone").slideToggle(500);
+            });
+        });
+        $(function() {
+            $(".close-zone").click(function(){
+                $(".zone").hide(500);
+            });
+        });
+        $(function() {
+            $(".tog5").click(function(){
+                $(".area").slideToggle(500);
+            });
+        });
+        $(function() {
+            $(".close-area").click(function(){
+                $(".area").hide(500);
+            });
+        });
+
+        $(function() {
+            $(".tog6").click(function(){
+                $(".parish").slideToggle(500);
+            });
+        });
+        $(function() {
+            $(".close-parish").click(function(){
+                $(".parish").hide(500);
+            });
+        });
